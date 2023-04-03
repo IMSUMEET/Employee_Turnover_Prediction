d62bdd6f04d8ca3d46ac85b9e962c79614b2f7ed
nikhjha <nikh.jha550@gmail.com>
2023-04-03 17:29:32 +0530

    Implementing validation for input form

--- client/src/pages/InputForm.jsx
+++ client/src/pages/InputForm.jsx
@@ -7,9 +7,92 @@ import Divider from "../components/Divider";
 import FileSelector from "../components/FileSelector";
 import SubmitButton from "../components/SubmitButton";
 import wwwFormURLencode from "../utils/urlencode";
+import validate from "validate.js";
+
+const salaryOptions = ["high", "medium", "low"];
+const departmentOptions = [
+  "IT",
+  "engineering",
+  "logistics",
+  "admin",
+  "finance",
+  "support",
+  "operations",
+  "marketing",
+];
+
+const constraint = {
+  performanceEval: {
+    presence: { allowEmpty: false },
+    numericality: {
+      onlyInteger: false,
+      greaterThanOrEqualTo: 0,
+      lessThanOrEqualTo: 1,
+    },
+  },
+  noOfProjects: {
+    presence: { allowEmpty: false },
+    numericality: {
+      onlyInteger: true,
+      greaterThanOrEqualTo: 0,
+    },
+  },
+  avgMonthlyHours: {
+    presence: { allowEmpty: false },
+    numericality: {
+      onlyInteger: true,
+      greaterThanOrEqualTo: 0,
+      lessThanOrEqualTo: 744,
+    },
+  },
+  noOfEmployeementYears: {
+    presence: { allowEmpty: false },
+    numericality: {
+      onlyInteger: true,
+      greaterThanOrEqualTo: 0,
+    },
+  },
+  bonusReceived: {
+    presence: { allowEmpty: false },
+    inclusion: {
+      within: ["1", "0"],
+      message: "^Select either 1 or 0 for bonus received",
+    },
+  },
+  promotionsIn5yrs: {
+    presence: { allowEmpty: false },
+    inclusion: {
+      within: ["1", "0"],
+      message: "^Select either 1 or 0 for promoted in last 5 yrs",
+    },
+  },
+  department: {
+    presence: { allowEmpty: false },
+    inclusion: {
+      within: departmentOptions,
+      message: "^Select from department options",
+    },
+  },
+  salaryLevel: {
+    presence: { allowEmpty: false },
+    inclusion: {
+      within: salaryOptions,
+      message: "^Select from salary options",
+    },
+  },
+  satisfactionLevel: {
+    presence: { allowEmpty: false },
+    numericality: {
+      onlyInteger: false,
+      greaterThanOrEqualTo: 0,
+      lessThanOrEqualTo: 1,
+    },
+  },
+};
 
 export default function InputForm({ onSubmit, onAuthFail, token }) {
   const [file, setFile] = useState();
+  const [message, setMessage] = useState("");
   //Handle form submittion
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -51,7 +134,7 @@ export default function InputForm({ onSubmit, onAuthFail, token }) {
         });
       } catch (e) {
         console.log(e);
-          onAuthFail();
+        onAuthFail();
       }
       return;
     }
@@ -67,6 +150,12 @@ export default function InputForm({ onSubmit, onAuthFail, token }) {
       salaryLevel: formData.get("sal"),
     };
     console.log(values);
+    const validation = validate(values, constraint, { format: "flat" });
+    if (validation && validation.length !== 0) {
+      setMessage(validation[0]);
+      return;
+    }
+    setMessage("");
     try {
       const form = {
         avg_hrs_month: values.avgMonthlyHours,
@@ -96,11 +185,27 @@ export default function InputForm({ onSubmit, onAuthFail, token }) {
       });
     } catch (e) {
       console.log(e);
-      onAuthFail()
+      onAuthFail();
     }
   };
   return (
     <div className="w-full px-40 py-10 bg-black text-teal-600">
+      {message !== "" && (
+        <div className="fixed w-screen top-0 left-0 p-4 flex justify-center">
+          <div className="rounded-lg bg-rose-600 text-white w-full max-w-[480px] py-2 px-4 flex justify-between items-center">
+            <p>{message}</p>
+            <div
+              className="w-[20px] h-[20px] rounded-full border border-white  relative cursor-pointer"
+              onClick={() => {
+                setMessage("");
+              }}
+            >
+              <div className="w-[2px] h-4/6 absolute top-1/2 left-1/2 bg-white -translate-y-1/2 -translate-x-1/2 rotate-45"></div>
+              <div className="w-[2px] h-4/6 absolute top-1/2 left-1/2 bg-white -translate-y-1/2 -translate-x-1/2 -rotate-45"></div>
+            </div>
+          </div>
+        </div>
+      )}
       <form className="w-full" noValidate onSubmit={handleSubmit}>
         <div className="w-full p-8 bg-white border-teal-600 border rounded-lg shadow shadow-teal-600">
           <Heading heading="Individual Evaluation" />
@@ -129,21 +234,12 @@ export default function InputForm({ onSubmit, onAuthFail, token }) {
           <CustomInputDropdownBox
             heading="Department"
             name="dept"
-            options={[
-              "IT",
-              "engineering",
-              "logistics",
-              "admin",
-              "finance",
-              "support",
-              "operations",
-              "marketing",
-            ]}
+            options={departmentOptions}
           />
           <CustomInputDropdownBox
             heading="Salary Level"
             name="sal"
-            options={["high", "medium", "low"]}
+            options={salaryOptions}
           />
         </div>
 
